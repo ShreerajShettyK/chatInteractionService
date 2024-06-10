6fde615eaf2293bf58dc2ae124406f84811385e9
shreerajshetty <rajshetty727@gmail.com>
2024-06-10 17:25:43 +0530

    auth token validated before profile token

--- go.mod
+++ go.mod
@@ -1,13 +1,24 @@
 module practicechat
 
-go 1.22
+go 1.22.3
 
+require github.com/IBM/sarama v1.43.2
 
 require (
-    github.com/IBM/sarama v1.43.2
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx v1.2.29 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 )
 
 require (
+	github.com/ShreerajShettyK/cognitoJwtAuthenticator v0.0.0-20240610090159-5e31968c51a6
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eapache/go-resiliency v1.6.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
--- internal/service/handlers.go
+++ internal/service/handlers.go
@@ -1,10 +1,14 @@
 package service
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/ShreerajShettyK/cognitoJwtAuthenticator"
 )
 
 // writeJSONResponse writes a JSON response with a given status code and message
@@ -21,9 +25,27 @@ func writeJSONResponse(w http.ResponseWriter, statusCode int, message string) {
 
 // SendMessageHandler is the HTTP handler for sending messages
 func SendMessageHandler(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		writeJSONResponse(w, http.StatusUnauthorized, "Missing Authorization token")
+		return
+	}
+
+	tokenString := authHeader[len("Bearer "):]
+	region := "us-east-1"
+	userPoolId := "us-east-1_icXeg2eiv"
+	ctx := context.Background()
+
+	_, err := cognitoJwtAuthenticator.ValidateToken(ctx, region, userPoolId, tokenString)
+	if err != nil {
+		writeJSONResponse(w, http.StatusUnauthorized, fmt.Sprintf("Token validation error: %s", err))
+		return
+	}
+
+	log.Println("Authorization token is valid")
 	profileToken := r.Header.Get("Profile-Token")
 	if profileToken == "" {
-		writeJSONResponse(w, http.StatusUnauthorized, "Missing tokens")
+		writeJSONResponse(w, http.StatusUnauthorized, "Missing Profile token")
 		return
 	}
 
@@ -38,6 +60,7 @@ func SendMessageHandler(w http.ResponseWriter, r *http.Request) {
 		writeJSONResponse(w, http.StatusBadRequest, "Failed to read request body")
 		return
 	}
+	log.Println("Profile token is valid")
 
 	var requestData map[string]string
 	err = json.Unmarshal(body, &requestData)
@@ -63,6 +86,7 @@ func SendMessageHandler(w http.ResponseWriter, r *http.Request) {
 		writeJSONResponse(w, http.StatusInternalServerError, fmt.Sprintf("Failed to send message: %v", err))
 		return
 	}
+	log.Println("Sent message to kafka")
 
 	w.Header().Set("Status", "200")
 	responseJSON := map[string]interface{}{
