1796cd6a32ef35b512a6d57407629b4ddfbc5e78
shreerajshetty <rajshetty727@gmail.com>
2024-06-12 12:41:39 +0530

    minor 2

--- internal/service/handlers.go
+++ internal/service/handlers.go
@@ -3,8 +3,7 @@ package service
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -24,73 +23,82 @@ func writeJSONResponse(w http.ResponseWriter, statusCode int, message string) {
 	w.Write(responseJSON)
 }
 
-// SendMessageHandler is the HTTP handler for sending messages
 func SendMessageHandler(w http.ResponseWriter, r *http.Request) {
+	var requestData map[string]string
 	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
+	if authHeader != "" {
+		authTokenString := authHeader[len("Bearer "):]
+		region := os.Getenv("REGION")
+		userPoolId := os.Getenv("COGNITO_USER_POOL_ID")
+		ctx := context.Background()
+
+		_, err := cognitoJwtAuthenticator.ValidateToken(ctx, region, userPoolId, authTokenString)
+		if err == nil {
+			log.Println("Authorization token is valid")
+
+			profileToken := r.Header.Get("Profile-Token")
+			if profileToken != "" {
+				claims, err := decodeJWT(profileToken)
+				if err == nil {
+					log.Println("Profile token is valid")
+
+					body, err := io.ReadAll(r.Body)
+					if err == nil {
+						err = json.Unmarshal(body, &requestData)
+						if err == nil {
+							// toExists and msgExists are boolean
+							to, toExists := requestData["to"]
+							message, msgExists := requestData["msg"]
+
+							if toExists && to != "" {
+								if msgExists && message != "" {
+									err = sendToKafka(claims.FirstName, to, message)
+									if err == nil {
+										writeJSONResponse(w, http.StatusOK, "Message sent successfully")
+										log.Println("--------------------------------------------------")
+										return
+									} else {
+										writeJSONResponse(w, http.StatusInternalServerError, err.Error())
+										log.Println("Failed to send message")
+										return
+									}
+								} else {
+									writeJSONResponse(w, http.StatusBadRequest, "Message is not set")
+									log.Println("Missing 'msg' parameter")
+									return
+								}
+							} else {
+								writeJSONResponse(w, http.StatusBadRequest, "Receiver not found")
+								log.Println("Missing 'to' parameter")
+								return
+							}
+						} else {
+							writeJSONResponse(w, http.StatusBadRequest, err.Error())
+							log.Println("Invalid JSON in request body")
+							return
+						}
+					} else {
+						writeJSONResponse(w, http.StatusBadRequest, err.Error())
+						log.Println("Failed to read request body")
+						return
+					}
+				} else {
+					writeJSONResponse(w, http.StatusUnauthorized, err.Error())
+					log.Println("Invalid Profile token")
+					return
+				}
+			} else {
+				writeJSONResponse(w, http.StatusUnauthorized, "Sender is not authorized")
+				log.Println("Missing Profile token")
+				return
+			}
+		} else {
+			writeJSONResponse(w, http.StatusUnauthorized, err.Error())
+			log.Println("Token validation error")
+			return
+		}
+	} else {
 		writeJSONResponse(w, http.StatusUnauthorized, "Missing Authorization token")
 		return
 	}
-
-	authTokenString := authHeader[len("Bearer "):]
-	region := os.Getenv("REGION")
-	userPoolId := os.Getenv("COGNITO_USER_POOL_ID")
-	ctx := context.Background()
-
-	_, err := cognitoJwtAuthenticator.ValidateToken(ctx, region, userPoolId, authTokenString)
-	if err != nil {
-		writeJSONResponse(w, http.StatusUnauthorized, fmt.Sprintf("Token validation error: %s", err))
-		return
-	}
-
-	log.Println("Authorization token is valid")
-
-	profileToken := r.Header.Get("Profile-Token")
-	if profileToken == "" {
-		writeJSONResponse(w, http.StatusUnauthorized, "Missing Profile token")
-		return
-	}
-
-	claims, err := decodeJWT(profileToken)
-	if err != nil {
-		writeJSONResponse(w, http.StatusUnauthorized, "Invalid profile token")
-		return
-	}
-	log.Println("Profile token is valid")
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		writeJSONResponse(w, http.StatusBadRequest, "Failed to read request body")
-		return
-	}
-
-	var requestData map[string]string
-	err = json.Unmarshal(body, &requestData)
-	if err != nil {
-		writeJSONResponse(w, http.StatusBadRequest, "Invalid JSON in request body")
-		return
-	}
-
-	//toExists and msgExists are boolean
-	to, toExists := requestData["to"]
-	message, msgExists := requestData["msg"]
-
-	if !toExists || to == "" {
-		writeJSONResponse(w, http.StatusBadRequest, "Missing 'to' parameter")
-		return
-	}
-	if !msgExists || message == "" {
-		writeJSONResponse(w, http.StatusBadRequest, "Missing 'msg' parameter")
-		return
-	}
-
-	err = sendToKafka(claims.FirstName, to, message)
-	if err != nil {
-		writeJSONResponse(w, http.StatusInternalServerError, fmt.Sprintf("Failed to send message: %v", err))
-		return
-	}
-
-	writeJSONResponse(w, http.StatusOK, "Message sent successfully")
-
-	log.Println("--------------------------------------------------")
 }
